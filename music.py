abcfe2721e7cde3c77ba9ac6bc2c4e9e2c61521c
Strijder513 <75573608+Strijder513@users.noreply.github.com>
2021-02-16 16:44:49 +0000

    Update music.py

--- music.py
+++ music.py
@@ -17,7 +17,6 @@ class MusicCog(commands.Cog):
     async def track_hook(self, event):
         if isinstance(event, lavalink.events.QueueEndEvent):
             guild_id = int(event.player.guild_id)
-            await self.connect_to(guild_id, None)
 
     async def connect_to(self, guild_id: int, channel_id: str):
         ws = self.bot._connection._get_websocket(guild_id)
@@ -93,6 +92,13 @@ class MusicCog(commands.Cog):
         player = self.bot.music.player_manager.get(ctx.guild.id)
         await player.set_pause(False)
         await message.edit(content="resumed")
+
+    @commands.command(name="skip")
+    async def skip(self, ctx):
+        message = await ctx.send("skipping song...")
+        player = self.bot.music.player_manager.get(ctx.guild.id)
+        await player.skip()
+        await message.edit(content="skipped.")
 
 
 def setup(bot):
